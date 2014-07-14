3550c68789641e257e9a6fc9791e653af27d8714
Antonio Forneron <>
2014-07-13 21:38:58 -0400

    jodiendo

--- src/forms/frmCiudades.java
+++ src/forms/frmCiudades.java
@@ -67,6 +67,11 @@ public class frmCiudades extends javax.swing.JFrame {
         jLabel2.setText("Nombre:");
 
         jButton1.setText("Guardar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Guardar");
 
@@ -152,6 +157,10 @@ public class frmCiudades extends javax.swing.JFrame {
         em.close();
         emf.close();
     }//GEN-LAST:event_formWindowClosed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
