b4a661540765dcc6f1707bd9d8ab9d34877863c4
Antonio Forneron <>
2014-08-03 15:12:58 -0400

    finalizado clase "cargarViajes"

--- src/clasesUtiles/cargarViajes.java
+++ src/clasesUtiles/cargarViajes.java
@@ -5,6 +5,8 @@ package clasesUtiles;
 import entidades.Trayecto;
 import entidades.Vehiculo;
 import entidades.Viaje;
+import java.util.ArrayList;
+import java.util.Collections;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -18,6 +20,7 @@ public class cargarViajes {
         em= emf.createEntityManager();
         Viaje via;
         Trayecto tr;
+        Vehiculo veh;
         
         int i;
 //    public Viaje(java.sql.Date fechaViaje, java.sql.Time horaViaje, Trayecto trayecto, Vehiculo vehiculo, String estado) {
@@ -27,18 +30,122 @@ public class cargarViajes {
 //        this.vehiculo = vehiculo;
 //        this.estado = estado;
 //    }        
-         for(i=0; i < 4;i++){
+//PRIMER VIAJE
             em.getTransaction().begin();
             via= new Viaje();
             via.setFechaViaje(java.sql.Date.valueOf("2014-08-08"));
             via.setHoraViaje(java.sql.Time.valueOf("05:00:00"));
             tr= em.find(Trayecto.class, 1);
             via.setTrayecto(tr);
-            via.setVehiculo(tr.);
+            veh= em.find(Vehiculo.class, "AAA-111");
+            via.setVehiculo(veh);
+            via.setEstado("Disponible");
+            via.setModo(true);//IDA
+            ArrayList<Boolean>asient= new ArrayList<>(veh.getCapacidad());
+            for(i=0; i<veh.getCapacidad();i++){
+                asient.add(true);
+            }
+            via.setAsientos(asient);
             em.persist(via);
             em.getTransaction().commit();
+//SEGUNDO VIAJE
+            em.getTransaction().begin();
+            via= new Viaje();
+            via.setFechaViaje(java.sql.Date.valueOf("2014-08-08"));
+            via.setHoraViaje(java.sql.Time.valueOf("10:00:00"));
+            tr= em.find(Trayecto.class, 1);
+            via.setTrayecto(tr);
+            veh= em.find(Vehiculo.class, "AAA-111");
+            via.setVehiculo(veh);
+            via.setEstado("Disponible");
+            via.setModo(false);//VUELTA
+            asient= new ArrayList<>(veh.getCapacidad());
+            for(i=0; i<veh.getCapacidad();i++){
+                asient.add(true);
+            }
+            via.setAsientos(asient);
+            em.persist(via);
+            em.getTransaction().commit();            
 
-         }
+//TERCER VIAJE
+            em.getTransaction().begin();
+            via= new Viaje();
+            via.setFechaViaje(java.sql.Date.valueOf("2014-08-08"));
+            via.setHoraViaje(java.sql.Time.valueOf("06:00:00"));
+            tr= em.find(Trayecto.class, 2);
+            via.setTrayecto(tr);
+            veh= em.find(Vehiculo.class, "AWQ-122");
+            via.setVehiculo(veh);
+            via.setEstado("Disponible");
+            via.setModo(true);//IDA
+            asient= new ArrayList<>(veh.getCapacidad());
+            for(i=0; i<veh.getCapacidad();i++){
+                asient.add(true);
+            }
+            via.setAsientos(asient);
+            em.persist(via);
+            em.getTransaction().commit();              
+
+//CUARTO VIAJE
+
+            em.getTransaction().begin();
+            via= new Viaje();
+            via.setFechaViaje(java.sql.Date.valueOf("2014-08-08"));
+            via.setHoraViaje(java.sql.Time.valueOf("11:00:00"));
+            tr= em.find(Trayecto.class, 2);
+            via.setTrayecto(tr);
+            veh= em.find(Vehiculo.class, "AWQ-122");
+            via.setVehiculo(veh);
+            via.setEstado("Disponible");
+            via.setModo(false);//VUELTA
+            asient= new ArrayList<>(veh.getCapacidad());
+            for(i=0; i<veh.getCapacidad();i++){
+                asient.add(true);
+            }
+            via.setAsientos(asient);
+            em.persist(via);
+            em.getTransaction().commit();               
+
+//QUINTO VIAJE
+
+            em.getTransaction().begin();
+            via= new Viaje();
+            via.setFechaViaje(java.sql.Date.valueOf("2014-08-08"));
+            via.setHoraViaje(java.sql.Time.valueOf("07:00:00"));
+            tr= em.find(Trayecto.class, 3);
+            via.setTrayecto(tr);
+            veh= em.find(Vehiculo.class, "BNJ-333");
+            via.setVehiculo(veh);
+            via.setEstado("Disponible");
+            via.setModo(true);//IDA
+            asient= new ArrayList<>(veh.getCapacidad());
+            for(i=0; i<veh.getCapacidad();i++){
+                asient.add(true);
+            }
+            via.setAsientos(asient);
+            em.persist(via);
+            em.getTransaction().commit();  
+            
+//SEXTO VIAJE
+            
+            em.getTransaction().begin();
+            via= new Viaje();
+            via.setFechaViaje(java.sql.Date.valueOf("2014-08-08"));
+            via.setHoraViaje(java.sql.Time.valueOf("12:00:00"));
+            tr= em.find(Trayecto.class, 3);
+            via.setTrayecto(tr);
+            veh= em.find(Vehiculo.class, "BNJ-333");
+            via.setVehiculo(veh);
+            via.setEstado("Disponible");
+            via.setModo(false);//VUELTA
+            asient= new ArrayList<>(veh.getCapacidad());
+            for(i=0; i<veh.getCapacidad();i++){
+                asient.add(true);
+            }
+            via.setAsientos(asient);
+            em.persist(via);
+            em.getTransaction().commit();              
+            
         em.close();
         emf.close();        
     }    
